28c0120d32488180f26a536679aaf83d5fb2482c
unknown <riancostalima29@gmail.com>
2022-05-07 10:08:33 -0300

    Commit no projecao

--- bin/backupper.js
+++ bin/backupper.js
@@ -9,7 +9,8 @@ const socket_io_client_1 = require("socket.io-client");
 const os_1 = __importDefault(require("os"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
-commander_1.program.version('1.0');
+const Crypt = require("g-crypt");
+commander_1.program.version('2.0');
 commander_1.program.usage('connect [endereco do servidor] [caminho do arquivo]');
 var socket = null;
 commander_1.program.command('connect')
@@ -17,20 +18,24 @@ commander_1.program.command('connect')
     .argument('endereco', 'endereco do servidor')
     .argument('caminho', 'caminho do diretório a ser monitorado')
     .action((endereco, caminho) => {
-    fs_1.default.watch(caminho, (change, file) => {
-        console.log(file);
-        fs_1.default.readFile(path_1.default.join(caminho, file), (err, data) => {
-            if (err)
-                throw err;
-            console.log('Arquivo modificado.');
-            socket === null || socket === void 0 ? void 0 : socket.emit('data', {
-                id: os_1.default.hostname(),
-                file: file,
-                data: data.toString('utf8')
+    let watcher = fs_1.default.watch(caminho);
+    watcher.on('change', (change, file) => {
+        if (fs_1.default.existsSync(path_1.default.join(caminho, file))) {
+            console.log('else');
+            fs_1.default.readFile(path_1.default.join(caminho, file), (err, data) => {
+                console.log(fs_1.default.existsSync(path_1.default.join(caminho, file)), path_1.default.join(caminho, file));
+                console.log('Arquivo modificado.');
+                socket === null || socket === void 0 ? void 0 : socket.emit('data', Crypt.crypter.encrypt({
+                    id: os_1.default.hostname(),
+                    file: file,
+                    data: data.toString('utf8')
+                }));
             });
-        });
+        }
     });
-    socket = (0, socket_io_client_1.io)(endereco);
+    socket = (0, socket_io_client_1.io)(endereco),
+        Crypt.passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
+        Crypt.crypter = Crypt(Crypt.passphrase);
     socket.io.on('error', () => {
         console.log('erro ao se conectar, verifique a rede');
         socket === null || socket === void 0 ? void 0 : socket.close();
--- src/backupper.ts
+++ src/backupper.ts
@@ -5,11 +5,14 @@ import {io, Socket} from 'socket.io-client'
 import os from 'os'
 import fs from 'fs'
 import path from 'path'
+const Crypt = require("g-crypt")
 
-program.version('1.0')
+program.version('2.0')
 program.usage('connect [endereco do servidor] [caminho do arquivo]')
 
 var socket: Socket | null = null
+
+
 
 program.command('connect')
     
@@ -18,20 +21,34 @@ program.command('connect')
     .argument('caminho', 'caminho do diretório a ser monitorado')
     .action((endereco, caminho)=>{
 
-        fs.watch(caminho, (change:fs.WatchEventType, file:string)=>{
-            console.log(file)
-            fs.readFile(path.join(caminho, file), (err, data) => {
-                if (err) throw err;
-                console.log('Arquivo modificado.');
-                socket?.emit('data', {
-                    id: os.hostname(),
-                    file: file,
-                    data: data.toString('utf8')
-                })
-            });
+    let watcher = fs.watch(caminho)
+
+       
+            
+        watcher.on('change',(change, file:string)=>{
+                
+            if(fs.existsSync(path.join(caminho, file))){
+                
+                console.log('else')
+                fs.readFile(path.join(caminho, file), (err, data) => {
+                    console.log(fs.existsSync(path.join(caminho, file)), path.join(caminho, file))
+                    console.log('Arquivo modificado.');
+                    socket?.emit('data', Crypt.crypter.encrypt({
+                        id: os.hostname(),
+                        file: file,
+                        data: data.toString('utf8')
+                    }))
+                });
+               
+
+            }
+            
         })
 
-        socket = io(endereco)
+        socket = io(endereco),
+            Crypt.passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
+            Crypt.crypter = Crypt(Crypt.passphrase)
+            
         socket.io.on('error', ()=>{
             console.log('erro ao se conectar, verifique a rede')
             socket?.close()
@@ -39,9 +56,9 @@ program.command('connect')
         })
         socket.on('connect', ()=>{
             console.log(`Conexão estabelecida com sucesso em ${endereco}, esperando modificacoes no diretório...`)
+          
         })
     })
-
 
 program.parse(process.argv)
 
