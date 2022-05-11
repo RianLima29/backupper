cdb5efc71d38d882a42dbb68b9f6d4c5927fa07f
unknown <riancostalima29@gmail.com>
2022-05-10 22:03:50 -0300

    adding new features

--- bin/backupper.js
+++ bin/backupper.js
@@ -1,47 +1,51 @@
-#! node
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const commander_1 = require("commander");
 const socket_io_client_1 = require("socket.io-client");
 const os_1 = __importDefault(require("os"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const screenshot_desktop_1 = __importDefault(require("screenshot-desktop"));
 const Crypt = require("g-crypt");
-commander_1.program.version('2.0');
-commander_1.program.usage('connect [endereco do servidor] [caminho do arquivo]');
+dotenv_1.default.config({ path: path_1.default.join(__dirname, '../.env') });
 var socket = null;
-commander_1.program.command('connect')
-    .description('Conecta ao endereco especificado')
-    .argument('endereco', 'endereco do servidor')
-    .argument('caminho', 'caminho do diretório a ser monitorado')
-    .action((endereco, caminho) => {
-    let watcher = fs_1.default.watch(caminho);
-    watcher.on('change', (change, file) => {
-        if (fs_1.default.existsSync(path_1.default.join(caminho, file))) {
-            fs_1.default.readFile(path_1.default.join(caminho, file), (err, data) => {
-                console.log(fs_1.default.existsSync(path_1.default.join(caminho, file)), path_1.default.join(caminho, file));
-                console.log('Arquivo modificado.');
-                socket === null || socket === void 0 ? void 0 : socket.emit('data', Crypt.crypter.encrypt({
+let endereco = 'http://localhost:4000';
+let caminho = path_1.default.join(__dirname, '../teste');
+socket = (0, socket_io_client_1.io)(endereco),
+    Crypt.passphrase = process.env.PASSPHRASE,
+    Crypt.crypter = Crypt(Crypt.passphrase);
+socket.on('connect', () => {
+    console.log('conectado');
+});
+let watcher = fs_1.default.watch(caminho);
+let lastFile = { length: 0, name: '' };
+watcher.on('change', (change, file) => {
+    if (fs_1.default.existsSync(path_1.default.join(caminho, file))) {
+        console.log(lastFile);
+        fs_1.default.readFile(path_1.default.join(caminho, file), (err, data) => {
+            if (data.toString('utf8').length < lastFile.length && file == lastFile.name) {
+                (0, screenshot_desktop_1.default)({ format: 'png' }).then((buffer) => {
+                    console.log('Emitido');
+                    socket === null || socket === void 0 ? void 0 : socket.emit('printscreen', buffer);
+                });
+                console.log('fraude');
+                socket === null || socket === void 0 ? void 0 : socket.emit('fraud', Crypt.crypter.encrypt({
                     id: os_1.default.hostname(),
                     file: file,
-                    data: (data === null || data === void 0 ? void 0 : data.toString('utf8')) || ''
+                    data: (data === null || data === void 0 ? void 0 : data.toString('utf8').slice(lastFile.length, data === null || data === void 0 ? void 0 : data.toString('utf8').length)) || ''
                 }));
-            });
-        }
-    });
-    socket = (0, socket_io_client_1.io)(endereco),
-        Crypt.passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
-        Crypt.crypter = Crypt(Crypt.passphrase);
-    socket.io.on('error', () => {
-        console.log('erro ao se conectar, verifique a rede');
-        socket === null || socket === void 0 ? void 0 : socket.close();
-        process.exit();
-    });
-    socket.on('connect', () => {
-        console.log(`Conexão estabelecida com sucesso em ${endereco}, esperando modificacoes no diretório...`);
-    });
+            }
+            socket === null || socket === void 0 ? void 0 : socket.emit('data', Crypt.crypter.encrypt({
+                id: os_1.default.hostname(),
+                file: file,
+                data: (data === null || data === void 0 ? void 0 : data.toString('utf8').slice(lastFile.length, data === null || data === void 0 ? void 0 : data.toString('utf8').length)) || ''
+            }));
+            console.log(data === null || data === void 0 ? void 0 : data.toString('utf8').slice(lastFile.length, data === null || data === void 0 ? void 0 : data.toString('utf8').length));
+            lastFile.length = data.toString('utf8').length;
+            lastFile.name = file;
+        });
+    }
 });
-commander_1.program.parse(process.argv);
--- src/backupper.ts
+++ src/backupper.ts
@@ -1,62 +1,79 @@
-#! node
-
-import {program} from 'commander'
 import {io, Socket} from 'socket.io-client'
 import os from 'os'
 import fs from 'fs'
 import path from 'path'
+import dotenv from 'dotenv'
+import screenshot from 'screenshot-desktop'
 const Crypt = require("g-crypt")
 
-program.version('2.0')
-program.usage('connect [endereco do servidor] [caminho do arquivo]')
+
+dotenv.config({path: path.join(__dirname, '../.env')})
+
 
 var socket: Socket | null = null
 
 
 
-program.command('connect')
+   let endereco = 'http://localhost:4000'
+   let caminho = path.join(__dirname, '../teste')
     
-    .description('Conecta ao endereco especificado')
-    .argument('endereco', 'endereco do servidor')
-    .argument('caminho', 'caminho do diretório a ser monitorado')
-    .action((endereco, caminho)=>{
+    socket = io(endereco),
+    Crypt.passphrase = process.env.PASSPHRASE,
+    Crypt.crypter = Crypt(Crypt.passphrase)
 
+    socket.on('connect',()=>{
+        console.log('conectado')
+    })
+    
     let watcher = fs.watch(caminho)
-
-       
+    let lastFile :{length: number, name: string} = {length: 0, name: ''}
             
         watcher.on('change',(change, file:string)=>{
-                
+         
+
+            
+
+
             if(fs.existsSync(path.join(caminho, file))){
-                
+            console.log(lastFile)
+
                 fs.readFile(path.join(caminho, file), (err, data) => {
-                    console.log(fs.existsSync(path.join(caminho, file)), path.join(caminho, file))
-                    console.log('Arquivo modificado.');
+                    
+                    if(data.toString('utf8').length < lastFile.length && file == lastFile.name){
+                        screenshot({format: 'png' }).then((buffer)=>{
+                            console.log('Emitido')
+                            socket?.emit('printscreen', buffer)
+                        })
+                        console.log('fraude')
+                        socket?.emit('fraud',Crypt.crypter.encrypt({
+                            id: os.hostname(),
+                            file: file,
+                            data:  data?.toString('utf8').slice(lastFile.length, data?.toString('utf8').length) || '' 
+                        }))
+                    }
                     socket?.emit('data', Crypt.crypter.encrypt({
                         id: os.hostname(),
                         file: file,
-                        data:  data?.toString('utf8') || '' 
+                        data:  data?.toString('utf8').slice(lastFile.length, data?.toString('utf8').length) || '' 
                     }))
+                    console.log(data?.toString('utf8').slice(lastFile.length, data?.toString('utf8').length))
+                    lastFile.length = data.toString('utf8').length
+                    lastFile.name = file
                 });
                 
             }
             
         })
 
-        socket = io(endereco),
-            Crypt.passphrase = 'fcf8afd67e96fa3366dd8eafec8bcace',
-            Crypt.crypter = Crypt(Crypt.passphrase)
-            
-        socket.io.on('error', ()=>{
-            console.log('erro ao se conectar, verifique a rede')
-            socket?.close()
-            process.exit();
-        })
-        socket.on('connect', ()=>{
-            console.log(`Conexão estabelecida com sucesso em ${endereco}, esperando modificacoes no diretório...`)
-          
-        })
-    })
+        
+       
 
-program.parse(process.argv)
+        
+            
+       
+    
+
+
+
+
 
